128c5423c036cff87327276fd29f54361e284cae
P.R. Neider-Ball <phillrballn@icloud.com>
2025-05-14 15:35:54 -0500

    created project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from '../Dashboard/Dashboard';
+import Reviews from '../Reviews/Reviews';
+import Aboutme from '../Aboutme/Aboutme';
 import "./App.css";
 import Header from "../Header/Header";
 
@@ -5,6 +9,11 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/aboutme" element={<Aboutme />} />
+      </Routes>
     </div>
   );
 }
