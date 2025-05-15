2544f4bd0d0c04b04ec707287125f5ab5f27068f
P.R. Neider-Ball <phillrballn@icloud.com>
2025-05-15 18:39:45 -0500

    updated project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
-import Header from "../Header/Header";
-import AboutUs from "../AboutUs/AboutUs";
+import Review from "../Review/Review";
 import AboutMe from "../AboutMe/AboutMe";
 import Contact from "../AboutMe/Contact";
 import Hobbies from "../AboutMe/Hobbies";
 import MyStory from "../AboutMe/MyStory";
+import AboutUs from "../AboutUs/AboutUs";
 import SiteHistory from "../AboutUs/SiteHistory";
 import SiteMission from "../AboutUs/SiteMission";
 import "./App.css";
@@ -25,13 +27,16 @@ function App() {
       })
       .catch(console.error);
   }, []);
-
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route
+          path="/reviews/:reviewId"
+          element={<Review reviews={reviews} />}
+        />
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
           <Route path="hobbies" element={<Hobbies />} />
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,15 +1,21 @@
-import './Review.css';
+import { Link } from "react-router-dom";
+import "./Reviews.css";
 
 function Reviews({ reviews }) {
   return (
     <div className="reviews">
       <ul className="reviews__list">
-        {reviews && reviews.map((review) => {
-          return <li key={review.id} className="reviews__item">{review.title}</li>
-        })}
+        {reviews &&
+          reviews.map((review) => {
+            return (
+              <li key={review.id} className="reviews__item">
+                <Link to={`${review.id}`}>{review.title}</Link>
+              </li>
+            );
+          })}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Reviews;
